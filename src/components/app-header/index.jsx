5aa818ab8970d72caca43df94f5831616baeabbb
Mrxiaov <51041769+Mrxiaov@users.noreply.github.com>
2021-11-03 16:30:10 +0800

    add header style

--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -1,13 +1,22 @@
 import React, { memo } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import { HeaderWrapper } from "./style";
+import { HeaderWrapper, HeaderLeft, HeaderRight } from "./style";
 
 export default memo(function HYAppHeader() {
   return (
     <HeaderWrapper>
-      <div className='content'></div>
+      <div className='content wrap-v1'>
+        <HeaderLeft >
+          <a href="#/" className="logo sprite_01"></a>
+        </HeaderLeft>
+        <HeaderRight ></HeaderRight>
+      </div>
       <div className='divider'></div>
+
+      <NavLink to='/'></NavLink>
+      <NavLink to='/mine'></NavLink>
+      <NavLink to='/friends'></NavLink>
     </HeaderWrapper>
   )
 })
--- src/components/app-header/style.js
+++ src/components/app-header/style.js
@@ -6,4 +6,27 @@ export const HeaderWrapper = styled.div`
   height: 75px;
   background-color: #242424;
   
+  .content {
+    height: 70px;
+    background-color: blue;
+    display: flex;
+    justify-content: space-between;
+  }
+  .divider {
+    height: 5px;
+    background-color: #c20c0c;
+  }
+`
+
+export const HeaderLeft = styled.div`
+  .logo {
+    display: block;
+    width: 176px;
+    height: 69px;
+    background-position: 0 0;
+  }
+`
+
+export const HeaderRight = styled.div`
+
 `
